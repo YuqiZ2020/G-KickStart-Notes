6071f75633d016ea48a6d19b3ce5341a1d9f72ec
Yuqi Zhang <59454965+YuqiZ2020@users.noreply.github.com>
2021-05-24 22:52:55 -0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+	RA21-Q1
+	All tests passed
+	Solution: 
+	Complexity:  
+*/
+
+void solve(int caseNum) 
+{
+	// Read in grids 
+	int r = 0; int c = 0; int t = 0;
+	cin >> r >> c;
+	vector<vector<int> > grid;
+	for (int i = 0; i < r; ++i)
+	{
+		vector<int> v;
+		for (int j = 0; j < c; ++j)
+		{
+			cin >> t;
+			v.push_back(t);
+		}
+		grid.push_back(v);
+	}
+} 
+
+int main()
+{
+	int T = 0;
+	cin >> T;
+	for (int i = 1; i <= T; ++i) 
+		solve(i);	
+}
